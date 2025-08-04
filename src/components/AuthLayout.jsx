8ca3cee1b353dbb7f1b2527b2026cdc1e2f1ca95
rajkamal3 <rkchenumalla@gmail.com>
2025-08-04 22:07:29 +0530

    auth loader added

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -5,6 +5,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { auth } from "@/lib/firebase";
 import Header from "@/components/Header";
+import "./AuthLayout.css";
 
 export default function AuthLayout({ children }) {
   const [user, setUser] = useState(null);
@@ -22,7 +23,12 @@ export default function AuthLayout({ children }) {
     return () => unsub();
   }, [router]);
 
-  if (!user) return <p>⏳ Loading the app...</p>;
+  if (!user)
+    return (
+      <div className="flex justify-center items-center h-[100vh]">
+        <div className="loader"></div>
+      </div>
+    );
 
   return (
     <div>
