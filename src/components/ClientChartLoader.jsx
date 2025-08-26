3fad5a8c69a7fb44cb008e2aaecff7c26493e5f5
rajkamal3 <rkchenumalla@gmail.com>
2025-08-26 19:23:24 +0530

    chart font size testing

--- src/components/ClientChartLoader.jsx
+++ src/components/ClientChartLoader.jsx
@@ -109,7 +109,7 @@ export default function Chart({ companyId }) {
       position: "start",
       backgroundColor: "rgba(0, 0, 0, 0.0)",
       color: "#000",
-      font: { size: 6 }
+      font: { size: 7 }
     }
   }));
 
